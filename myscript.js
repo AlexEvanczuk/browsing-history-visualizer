0852bbe94ab22214c6299afd8d534d27a52a3a09
Alex Evanczuk <alexevanczuk@alexevanczuk.local>
2013-12-25 16:56:11 -0500

    Procfile

---
+++ myscript.js
@@ -0,0 +1,45 @@
+// Use background page console
+var bkg = chrome.extension.getBackgroundPage();
+
+// Keep track of the current page URL
+var current = ''
+// Keep track of time on current page
+var n = new Date().getTime();
+
+// Use onUpdated to check for an active tab navigation change
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if(tab.active){
+    	var url = changeInfo.url;
+    	if(url != undefined){
+    		newTime = new Date().getTime();
+    		bkg.console.log(current + " (" + (newTime - n) + ")\n" + url);
+    		n = newTime;
+    		current = url;
+    	}
+    }
+});
+
+// Keep this method in case I want to check open, inactive tabs
+/*
+chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {         
+   bkg.console.log("Created: " + changeInfo.url)
+});
+*/
+
+// Use onActivated to check for navigating to a new tabrefer to use onactivated -- as it only logs the urls when the 
+// particular tab is opened and activated
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+	chrome.tabs.query({'active': true, 'currentWindow': true}, function(tabs) {
+		var url = tabs[0].url;
+
+		// Do not log on initial tab activation
+		if(current.valueOf() == ''){
+			current = url;
+		} else {
+    		newTime = new Date().getTime();
+    		bkg.console.log(current + " (" + (newTime - n) + ")\n" + url);
+    		n = newTime;
+    		current = url;
+		}
+	});
+});
